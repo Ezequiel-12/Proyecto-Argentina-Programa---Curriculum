d96c4f0f92c310ad77e7f33455590a124cbf9f69
ezequiel <eze-leo11@hotmail.com>
2023-02-28 14:54:45 -0300

    lol

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let buttonFunction =document.querySelector('[data-button]');
+let textFunction =document.querySelector('[data-showmore]');
+
+let buttonState = 0;
+buttonFunction.addEventListener('click', function(){
+    if (buttonState === 0) {
+        buttonFunction.style.marginBottom = 0;
+        textFunction.classList.remove('hidden');
+        setTimeout(function(){
+            textFunction.classList.add('shown');
+            buttonFunction.innerHTML = 'Show Less';
+        }, 200);
+        buttonState = 1;
+        console.log(buttonState)
+    } else if (buttonState === 1) {
+        textFunction.classList.remove('shown');
+        setTimeout(function(){
+            textFunction.classList.add('hidden');
+            buttonFunction.innerHTML = 'Show More';
+        }, 400);
+        buttonState = 0;
+        console.log(buttonState)
+    }
+})
